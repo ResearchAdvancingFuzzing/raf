b7c2f2506249268e6c1fca57b2c5ca2c23aea40b
Andy <andy@bigandian.com>
2020-01-17 18:00:16 -0500

    beginning drcov integration

--- spitfire/tools/mutfuzz/cov2api.py
+++ spitfire/tools/mutfuzz/cov2api.py
@@ -1,18 +1,39 @@
 import drcov
-import sys
+import logging
+import grpc
+import hydra
 import os
 import os.path
 
-if __name__ == "__main__":
-    argc = len(sys.argv)
-    argv = sys.argv
+import spitfire.protos.knowledge_base_pb2 as pb
+import spitfire.protos.knowledge_base_pb2_grpc as rpc
 
-    # base usage
-    if argc < 2:
-        print "usage: %s <coverage filename>" % os.path.basename(sys.argv[0])
-        sys.exit()
+@hydra.main(config_path="../../config/expt1/config.yaml")
+def run(cfg):
+    
+    with grpc.insecure_channel('%s:%d' % (cfg.kb_host, cfg.kb_port)) as channel:
+        stub = rpc.KnowledgeBaseStub(channel)
 
-    # attempt file parse
-    x = drcov.DrcovData(argv[1])
-    for bb in x.basic_blocks:
-        print "0x%08x\t%s @ 0x%08x" % (bb.start, x.modules[bb.mod_id].filename, x.modules[bb.mod_id].base)
+        for cov_file in os.listdir(cfg.coverage.coverage_directory):
+            cov_path = os.path.join(cfg.coverage.coverage_directory, cov_file)
+            cov = drcov.DrcovData(cov_path)
+
+            modules = []
+            for m in cov.modules:
+                modules[m.id] = pb.Module(name=m.filename, base=m.base, end=m.end, filepath=m.path)
+
+            i = 0
+            for r in stub.AddModules(iter(modules)):
+                modules[i] = r
+                i += 1
+
+            basic_blocks = []
+                
+            for bb in cov.basic_blocks:
+                basic_blocks.append(pb.Address(module=modules[bb.mod_id], offset=bb.start))
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
