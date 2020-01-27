b16090828d8cdb6a659cc08f6e31216c05dfec6f
Andy <andy@bigandian.com>
2020-01-27 17:30:39 -0500

    python 3 fixes

--- spitfire/tools/mutfuzz/cov2api.py
+++ spitfire/tools/mutfuzz/cov2api.py
@@ -4,6 +4,20 @@ import grpc
 import hydra
 import os
 import os.path
+import sys
+
+
+p = os.path.abspath(__file__)
+for i in range(10):
+    (hd, tl) = os.path.split(p)
+    if tl == "spitfire":
+        print("adding path " + p)
+        print("adding path " + hd)
+        sys.path.append(p)
+        sys.path.append(hd)
+        sys.path.append(p + "/protos")
+        break
+    p = hd
 
 import spitfire.protos.knowledge_base_pb2 as pb
 import spitfire.protos.knowledge_base_pb2_grpc as rpc
@@ -13,26 +27,33 @@ def run(cfg):
     
     with grpc.insecure_channel('%s:%d' % (cfg.kb_host, cfg.kb_port)) as channel:
         stub = rpc.KnowledgeBaseStub(channel)
-
         for cov_file in os.listdir(cfg.coverage.coverage_directory):
             cov_path = os.path.join(cfg.coverage.coverage_directory, cov_file)
+
+            print("parsing coverage file: " + cov_path)            
+
             cov = drcov.DrcovData(cov_path)
 
             modules = []
             for m in cov.modules:
-                modules[m.id] = pb.Module(name=m.filename, base=m.base, end=m.end, filepath=m.path)
+                modules.insert(m.id, pb.Module(name=m.filename.strip(), base=m.base, end=m.end, filepath=m.path.strip()))
 
             i = 0
             for r in stub.AddModules(iter(modules)):
                 modules[i] = r
                 i += 1
-
-            basic_blocks = []
                 
+            basic_blocks = []
+            
             for bb in cov.basic_blocks:
                 basic_blocks.append(pb.Address(module=modules[bb.mod_id], offset=bb.start))
+                
+            i = 0
+            for r in stub.AddAddresses(iter(basic_blocks)):
+                basic_blocks[i] = r
+                i += 1
 
-
+            
 
 if __name__ == '__main__':
     logging.basicConfig()
