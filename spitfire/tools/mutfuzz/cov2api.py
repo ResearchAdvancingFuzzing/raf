f5051defae3000f92b36e021c71070628218a019
Andy <andy@bigandian.com>
2020-01-30 12:09:31 -0500

    cov2api.py now works, but we may want to move some things around

--- spitfire/tools/mutfuzz/cov2api.py
+++ spitfire/tools/mutfuzz/cov2api.py
@@ -5,14 +5,12 @@ import hydra
 import os
 import os.path
 import sys
-
+from collections import Counter
 
 p = os.path.abspath(__file__)
 for i in range(10):
     (hd, tl) = os.path.split(p)
     if tl == "spitfire":
-        print("adding path " + p)
-        print("adding path " + hd)
         sys.path.append(p)
         sys.path.append(hd)
         sys.path.append(p + "/protos")
@@ -21,19 +19,19 @@ for i in range(10):
 
 import spitfire.protos.knowledge_base_pb2 as pb
 import spitfire.protos.knowledge_base_pb2_grpc as rpc
+import google.protobuf.json_format
 
 @hydra.main(config_path="../../config/expt1/config.yaml")
-def run(cfg):
-    
+def run(cfg):    
     with grpc.insecure_channel('%s:%d' % (cfg.kb_host, cfg.kb_port)) as channel:
         stub = rpc.KnowledgeBaseStub(channel)
-        for cov_file in os.listdir(cfg.coverage.coverage_directory):
-            cov_path = os.path.join(cfg.coverage.coverage_directory, cov_file)
-
-            print("parsing coverage file: " + cov_path)            
-
-            cov = drcov.DrcovData(cov_path)
-
+        for cov_file in os.scandir(cfg.coverage.coverage_directory):
+            if not cov_file.is_file() or not cov_file.name.endswith('.coverage'):
+                continue
+            
+            cov = drcov.DrcovData(cov_file.path)
+            cov_input = pb.Input(filepath=cov_file.path.replace('.coverage', '.input'))
+            stub.AddInput(cov_input)
             modules = []
             for m in cov.modules:
                 modules.insert(m.id, pb.Module(name=m.filename.strip(), base=m.base, end=m.end, filepath=m.path.strip()))
@@ -44,7 +42,7 @@ def run(cfg):
                 i += 1
                 
             basic_blocks = []
-            
+
             for bb in cov.basic_blocks:
                 basic_blocks.append(pb.Address(module=modules[bb.mod_id], offset=bb.start))
                 
@@ -53,8 +51,22 @@ def run(cfg):
                 basic_blocks[i] = r
                 i += 1
 
+            basic_blocks = map(google.protobuf.json_format.MessageToJson, basic_blocks)
+            # https://docs.python.org/3.6/library/functions.html#zip
+            edge_tuples = zip(*[iter(basic_blocks)]*cfg.coverage.number_of_addresses)
+            edge_count = Counter(edge_tuples)
+            edges = []
+            def json_to_addr(json):
+                return google.protobuf.json_format.Parse(json, pb.Address())
             
+            for e in edge_count:
+                msg_e = map(json_to_addr, e)
+                edges.append(pb.EdgeCoverage(hit_count=edge_count[e], address=msg_e, input=cov_input))
 
+            for r in stub.AddEdgeCoverage(iter(edges)):
+                print(r.uuid)
+
+            
 if __name__ == '__main__':
     logging.basicConfig()
     run()
