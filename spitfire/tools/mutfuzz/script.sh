8556220816b15c307cf7adb9d82bb2f308a0ead1
Heather Preslier <heatherpreslier@hotmail.com>
2020-02-20 15:14:49 -0500

    Added input persistent volume

--- spitfire/tools/mutfuzz/script.sh
+++ spitfire/tools/mutfuzz/script.sh
@@ -2,6 +2,8 @@
 
 # Script to run gtfo and drcov and output .coverage files 
 
+# This needs to be environment variables in the container not in here
+
 export WORK_DIR="/gtfo-source" #"/home/he27553" #"/gtfo-source"
 export TARGET_DIR="/target" #"/usr/local/bin" #"/target"
 export CORPUS_DIR="/seed-corpus"
@@ -29,6 +31,10 @@ mkdir /python
 cd /python
 LD_LIBRARY_PATH=$WORK_DIR/gtfo/lib/ ANALYSIS_SIZE=65536 JIG_MAP_SIZE=65536 JIG_TARGET=$TARGET_DIR/tiff2rgba  JIG_TARGET_ARGV="-c jpeg fuzzfile /dev/null" $WORK_DIR/gtfo/bin/the_fuzz -S $WORK_DIR/gtfo/gtfo/analysis/afl_bitmap_analysis.so -O $WORK_DIR/gtfo/gtfo/ooze/afl_havoc.so -J $WORK_DIR/gtfo/gtfo/the_fuzz/afl_jig.so -i $CORPUS_DIR/not_kitty.tiff -n 1000 -x 1024 -c bitmap -s `head -c 10 /dev/urandom | xxd -p`
 
+# For now lets just put it all into the inputs directory 
+# We can separate later 
+cp /python/interesting/crash/* /inputs
+cp /python/coverage/* /inputs
 
 run_drcov "/python/interesting/crash"
 run_drcov "/python/coverage"
