b18b15cd75309f863f1bb1658ce415f98db02bcf
Heather Preslier <heatherpreslier@Heathers-MacBook-Air.local>
2020-02-21 06:45:59 -0500

    Fixed some bugs in the run script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM ubuntu
 
 COPY spitfire /tmp
-CMD cp -r /tmp/* /spitfire
+CMD cp -r /tmp/* /spitfire 
+#$SPITFIRE
+#&& cp -r /tmp/* /shared/spitfire
+#$SPITFIRE
+#/spitfire
 
 #COPY spitfire/protos /tmp 
 #CMD cp /tmp /spitfire/protos 
--- spitfire/tools/mutfuzz/get_deps.sh
+++ spitfire/tools/mutfuzz/get_deps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/gaasedelen/lighthouse/master/plugin/lighthouse/parsers/drcov.py > drcov.py
-patch drcov.py <drcov.py.patch 
+patch drcov.py < spitfire/tools/mutfuzz/drcov.py.patch
+mv drcov.py spitfire/tools/mutfuzz
