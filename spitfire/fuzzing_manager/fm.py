295a248a8110f23bf62c10e3f056c9481155f7b9
Heather Preslier <heatherpreslier@hotmail.com>
2020-03-26 17:35:20 -0400

    Renamed job scripts to the same name. Working on fm. Not much of this has been tested.

--- spitfire/fuzzing_manager/fm.py
+++ spitfire/fuzzing_manager/fm.py
@@ -7,7 +7,9 @@ from kubernetes import client, utils, config
 import os
 import yaml
 import time
-import random 
+import random
+import hydra
+import logging
 # some guess at how much time we'll spend on each of these
 P_SEED_MUTATIONAL_FUZZ = 0.25
 P_COVERAGE_FUZZ = 0.25
@@ -18,14 +20,37 @@ P_TAINT_ANALYSIS = 0.25
 # so, like 10 cores or nodes or whatever
 budget = 10 
 
+def create_job_from_yaml(api_instance, num, commands, template_file): 
+    with open( template_file ) as f:
+        job=yaml.safe_load(f)
+        #print(job)
+        name ="%s-%s" % (job["metadata"]["name"], str(num)) 
+        job["metadata"]["name"] = name
+        job["spec"]["template"]["metadata"]["name"] = name
+        job["spec"]["template"]["spec"]["containers"][0]["name"]=name
+        job["spec"]["template"]["spec"]["containers"][0]["command"]=commands
+	#job["spec"]["template"]["metadata"]["labels"]["app"]=name
+	#job["spec"]["template"]["spec"]["containers"][0]["image"]=image
+	#job["spec"]["template"]["spec"]["containers"][0]["command"]=commands
+	#job["spec"]["template"]["spec"]["containers"][0]["env"]=envs
+    #print(job)
+    api_response = api_instance.create_namespaced_job(body=job, namespace="default")
+    print("Job created. status='%s'" % str(api_response.status))
+    return job
+
 # somehow Heather runs this fn in a kubernetes cron job every M minutes
 # M=5 ?
 # this cfg is the hydra thing, I hope
-@hydra.main(config_path=f"{spitfire_dir}/config/expt1/config.yaml")
+cur_dir = "/home/hpreslier/raf/spitfire"
+@hydra.main(config_path=f"{cur_dir}/config/expt1/config.yaml")
+#@hydra.main(config_path=f"{spitfire_dir}/config/expt1/config.yaml")
 def run(cfg):
 
     #N = consult kubernetes to figure out how much many cores we are using currently
-
+    config.load_kube_config()
+    batch_v1 = client.BatchV1Api() 
+    create_job_from_yaml(batch_v1, 1, "python3.6 run.py", f"{cur_dir}/fuzzing_manager/jobs/config_coverage.yaml")  
+    return
     #if N >= budget:
         # we are using all the compute we have -- wait
     #    exit()
@@ -51,7 +76,7 @@ def run(cfg):
 
             # set of seed inputs we have not yet fuzzed
             RS = S - F
-            if |RS| == 0:
+            if abs(RS) == 0:
                 # seed fuzzing not possible -- try something else 
                 continue
 
@@ -72,7 +97,7 @@ def run(cfg):
 
             # set of inputs for which we have coverage info but have not yet fuzzed
             RC = C - F
-            if |RC| == 0:
+            if abs(RC) == 0:
                 # covg based fuzzing not possible -- try something else 
                 continue
 
@@ -94,7 +119,7 @@ def run(cfg):
 
             # inputs for which we have taint info AND haven't yet fuzzed
             RT = T - F
-            if |RT| == 0:
+            if abs(RT) == 0:
                 # taint based fuzzing not possible -- try something else
                 continue
 
@@ -104,7 +129,8 @@ def run(cfg):
 
             # Now we need to make some actual reccomendations to fuzzer
             # in order that it can make use of taint info.
-            fbss = Consult knowledge base taint info to get Fbs for t that taint fewer than MAX_TAINT_OUT_DEGREE instructions
+            
+            #fbss = Consult knowledge base taint info to get Fbs for t that taint fewer than MAX_TAINT_OUT_DEGREE instructions
             fbs_to_fuzz = set([])
             for fbs in fbss:
                 if len(taint_mappings_for_fbs(fbs)) < MAX_TAINT_IN_DEGREE:
@@ -114,7 +140,7 @@ def run(cfg):
             # NB: We need a new version of GTFO that can be informed by this, i.e., that 
             # can take as input a set of fuzzable byte sets to fuzz in a focused way 
             gtfo_taint(t, fbs_to_fuzz, timeout=cfg.mutfuzz.timeout)
-            tell knowledge base to add s to F?  Or maybe gtfo does that
+            #tell knowledge base to add s to F?  Or maybe gtfo does that
             # cron job finished 
             exit()
 
@@ -131,7 +157,7 @@ def run(cfg):
             # gotta be a better way maybe using covg?
             t = random.random(IS)
             panda_taint(t)
-            tell knowledge base to add t to T?  Or maybe panda taint does that
+            #tell knowledge base to add t to T?  Or maybe panda taint does that
             # cron job finished
             exit()
 
