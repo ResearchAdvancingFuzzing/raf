ae2a87e71ccbd51311aad02f6729bc9efe6d5945
Heather Preslier <heatherpreslier@hotmail.com>
2020-08-18 13:49:22 -0400

    Making the suspend script for namespaces

--- suspend.sh
+++ suspend.sh
@@ -1,1 +1,10 @@
-kubectl patch cronjob fm -p '{"spec" : {"suspend" : true }}'
+#!/bin/bash
+
+namespace=$1
+if [ -z "$namespace" ]
+then 
+    echo "Usage: ./suspend.sh <campaign-id>"
+    exit 1
+fi
+
+kubectl patch cronjob fm --namespace="$namespace" -p '{"spec" : {"suspend" : true }}'
