7f5f47ec2ec8a323de8dd8d6c62562fd7b943f51
Heather Preslier <heatherpreslier@hotmail.com>
2020-09-21 14:00:47 -0400

    Fixed panda dockerfile

--- build.sh
+++ build.sh
@@ -27,17 +27,18 @@ set -e
 cd spitfire/protos && make clean && make && cd ../..
 
 # Rebuild docker images 
+docker build -t gtfo-source:v1 -f spitfire/tools/gtfo-source/Dockerfile .
+docker build -t spitfirepanda -f ./Dockerfile_panda .
+docker build -t spitfirepanda:python -f ./Dockerfile_18_04 .
 docker build -t raf -f ./Dockerfile_raf . 
+
 docker build -t init:$id -f spitfire/init/Dockerfile_init .
 docker build -t fm:$id -f spitfire/fuzzing_manager/Dockerfile .
-docker build -t gtfo-source:v1 -f spitfire/tools/gtfo-source/Dockerfile .
 docker build -t seed-corpus:$id -f spitfire/init/Dockerfile_corpus .
 docker build -t target:$id -f spitfire/init/Dockerfile_target .
 docker build -t spitfire:$id .
 docker build -t fuzzer:$id -f spitfire/components/mutfuzz/Dockerfile .
 docker build -t knowledge-base:$id -f spitfire/knowledge_base/Dockerfile .
-#docker build -t spitfirepanda -f ./Dockerfile_panda .
-#docker build -t spitfirepanda:python -f ./Dockerfile_18_04 .
 docker build -t taint:$id -f spitfire/components/taint/panda/Dockerfile_taint .
 docker build -t coverage:$id -f spitfire/components/coverage/Dockerfile .
 
